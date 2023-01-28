1f66c2763574abe6624b3cc5759707d1a7786faf
Jan Chudy <chudyja1@fit.cvut.cz>
2023-01-28 19:54:53 +0100

    Modified the rviz launch file to support xacro robot descriptions.

--- launch/rviz_launch.py
+++ launch/rviz_launch.py
@@ -1,12 +1,15 @@
 import os
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.substitutions import Command
 
 from ament_index_python.packages import get_package_share_directory
 
-URDF_FILE = "dummy_arm.urdf"
-RVIZ_CONFIG_FILE = "urdf_viz.rviz"
+# URDF_FILE = "dummy_arm.urdf"
+# URDF_FILE = "arduinobot.urdf.xacro"
+URDF_FILE = "rr1.urdf.xacro"
 
+RVIZ_CONFIG_FILE = "urdf_viz.rviz"
 BRINGUP_PKG = "rr1_bringup"
 DESCRIPTION_PKG = "rr1_description"
 
@@ -23,7 +26,7 @@ def generate_launch_description():
         package="robot_state_publisher",
         executable="robot_state_publisher",
         output="both",
-        arguments=[urdf_path]
+        parameters=[{'robot_description': Command(['xacro ', urdf_path])}]
     )
 
     rviz_config = os.path.join(get_package_share_directory(BRINGUP_PKG), 
